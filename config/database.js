e77b8e309462d51b2dc8b56514c095b154ed724d
Anisuzzaman Khan <aniskhan001@outlook.com>
2017-04-12 17:35:35 +0600

    db config

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://aniskhan001:pointhacks@ds159180.mlab.com:59180/heroku_7td1p4h2' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
